d22e9085c384b58958361721e65e629c7e17081d
Stefan Kangas <skangas@skangas.se>
2011-12-24 06:34:58 +0100

    update

--- .emacs.d/my-general.el
+++ .emacs.d/my-general.el
@@ -279,12 +279,12 @@
 
 (setq lazy-highlight-initial-delay 0.1)
 
-
+;;; abbreviate mode names
 (when (require 'diminish nil 'noerror)
-  (eval-after-load "company"
-      '(diminish 'company-mode "Cmp"))
   (eval-after-load "abbrev"
     '(diminish 'abbrev-mode "Ab"))
+  (eval-after-load "company"
+    '(diminish 'company-mode "Cmp"))
   (eval-after-load "yasnippet"
     '(diminish 'yas/minor-mode "Y")))
 
