e02f084673c539bfe7a2c5dd008253a920f591a0
Stefan Kangas <skangas@skangas.se>
2012-01-07 00:45:10 +0100

    update scheme config

--- .emacs.d/my-coding-common-lisp.el
+++ .emacs.d/my-coding-common-lisp.el
@@ -5,12 +5,6 @@
 (defun my-lisp-mode-customizations ()
   (my-coding-keys lisp-mode-base-map))
 
-;; Scheme
-
-(add-hook 'scheme-mode-hook 'my-scheme-mode-customizations t)
-(defun my-scheme-mode-customizations ()
-  (my-coding-keys scheme-mode-map))
-
 (provide 'my-coding-common-lisp)
 
 ;; my-coding-common-lisp.el ends here
