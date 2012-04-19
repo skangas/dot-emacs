2a3946492d17711c1e9f3e0ec227f9e1e4311a17
Stefan Kangas <skangas@skangas.se>
2012-04-19 11:00:14 +0200

    automatic update @ huey

--- .emacs.d/my-org-mode.el
+++ .emacs.d/my-org-mode.el
@@ -95,6 +95,16 @@
      (setq org-stuck-projects (quote ("" nil nil "")))
 
      ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+     ;; ;; reftex
+     (defun org-mode-reftex-setup ()
+       (load-library "reftex")
+       (and (buffer-file-name)
+            (file-exists-p (buffer-file-name))
+            (reftex-parse-all))
+       (define-key org-mode-map (kbd "C-c )") 'reftex-citation))
+     (add-hook 'org-mode-hook 'org-mode-reftex-setup)
+
+     ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
      ;; ;; remember.el
 
      (org-remember-insinuate)
