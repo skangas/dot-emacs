32404d13702308416cc3e248ad56f492dc5aa4c5
Stefan Kangas <stefan.kangas@ericsson.com>
2013-05-13 12:55:32 +0200

    update auto-complete to 1.4
    Change-Id: I4cf138cce69beb9ebdd382b70a212cad3a731507

--- .emacs.d/elpa/auto-complete-1.4/auto-complete-config.el
+++ .emacs.d/elpa/auto-complete-1.4/auto-complete-config.el
@@ -4,7 +4,7 @@
 
 ;; Author: Tomohiro Matsuyama <m2ym.pub@gmail.com>
 ;; Keywords: convenience
-;; Version: 1.3.1
+;; Version: 1.4
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -90,7 +90,7 @@
 
 (defun ac-gtags-candidate ()
   (ignore-errors
-    (split-string (shell-command-to-string (format "global -ci %s" ac-prefix)) "\n")))
+    (split-string (shell-command-to-string (format "global -ciq %s" ac-prefix)) "\n")))
 
 (ac-define-source gtags
   '((candidates . ac-gtags-candidate)
@@ -368,29 +368,44 @@
   "Current editing property.")
 
 (defun ac-css-prefix ()
-  (when (save-excursion (re-search-backward "\\_<\\(.+?\\)\\_>\\s *:.*\\=" nil t))
+  (when (save-excursion (re-search-backward "\\_<\\(.+?\\)\\_>\\s *:[^;]*\\=" nil t))
     (setq ac-css-property (match-string 1))
     (or (ac-prefix-symbol) (point))))
 
 (defun ac-css-property-candidates ()
-  (or (loop with list = (assoc-default ac-css-property ac-css-property-alist)
-            with seen = nil
-            with value
-            while (setq value (pop list))
-            if (symbolp value)
-            do (unless (memq value seen)
-                 (push value seen)
-                 (setq list
-                       (append list
-                               (or (assoc-default value ac-css-value-classes)
-                                   (assoc-default (symbol-name value) ac-css-property-alist)))))
-            else collect value)
-      ac-css-pseudo-classes))
+  (let ((list (assoc-default ac-css-property ac-css-property-alist)))
+    (if list
+        (loop with seen
+              with value
+              while (setq value (pop list))
+              if (symbolp value)
+              do (unless (memq value seen)
+                   (push value seen)
+                   (setq list
+                         (append list
+                                 (or (assoc-default value ac-css-value-classes)
+                                     (assoc-default (symbol-name value) ac-css-property-alist)))))
+              else collect value)
+      ac-css-pseudo-classes)))
 
-(defvar ac-source-css-property
+(ac-define-source css-property
   '((candidates . ac-css-property-candidates)
     (prefix . ac-css-prefix)
     (requires . 0)))
+
+;; slime
+(ac-define-source slime
+  '((depends slime)
+    (candidates . (car (slime-simple-completions ac-prefix)))
+    (symbol . "s")
+    (cache)))
+
+;; ghc-mod
+(ac-define-source ghc-mod
+  '((depends ghc)
+    (candidates . (ghc-select-completion-symbol))
+    (symbol . "s")
+    (cache)))
 
 
 
@@ -455,7 +470,8 @@
 ;;;; Default settings
 
 (defun ac-common-setup ()
-  (add-to-list 'ac-sources 'ac-source-filename))
+  ;(add-to-list 'ac-sources 'ac-source-filename)
+  )
 
 (defun ac-emacs-lisp-mode-setup ()
   (setq ac-sources (append '(ac-source-features ac-source-functions ac-source-yasnippet ac-source-variables ac-source-symbols) ac-sources)))
@@ -463,9 +479,7 @@
 (defun ac-cc-mode-setup ()
   (setq ac-sources (append '(ac-source-yasnippet ac-source-gtags) ac-sources)))
 
-(defun ac-ruby-mode-setup ()
-  (make-local-variable 'ac-ignores)
-  (add-to-list 'ac-ignores "end"))
+(defun ac-ruby-mode-setup ())
 
 (defun ac-css-mode-setup ()
   (setq ac-sources (append '(ac-source-css-property) ac-sources)))
