12ab828249e3602ac686c95264f380ebc90f0bdc
Stefan Kangas <stefan.kangas@ericsson.com>
2013-05-23 11:58:14 +0200

    Remove own version of count-words
    Change-Id: I8a1edb5e67f931384ea03643f7eba2fcf978d99e

--- .emacs.d/lisp-personal/sk-lisp.el
+++ .emacs.d/lisp-personal/sk-lisp.el
@@ -107,14 +107,4 @@
             (push line lines)
             (forward-line 1)))))))
 
-;; count words function
-(defun count-words (start end)
-  "Print number of words in the region."
-  (interactive "r")
-  (save-excursion
-    (save-restriction
-      (narrow-to-region start end)
-      (goto-char (point-min))
-      (message (format "%d" (count-matches "\\sw+"))))))
-
 (provide 'sk-lisp)
