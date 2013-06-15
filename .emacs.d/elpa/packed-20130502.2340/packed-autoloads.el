0a9e4954d2298522e223ae4a9c5791fdc95d2fa2
Stefan Kangas <skangas@skangas.se>
2013-06-15 17:06:11 +0200

    Add auto-complie

---
+++ .emacs.d/elpa/packed-20130502.2340/packed-autoloads.el
@@ -0,0 +1,13 @@
+;;; packed-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+(provide 'packed-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; packed-autoloads.el ends here
