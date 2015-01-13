38358af2f2de8f2fa63357986798e3712ebf31a4
Stefan Kangas <skangas@skangas.se>
2015-01-13 13:30:15 +0100

    ELPA upgrade

--- .emacs.d/elpa/cursor-chg-20131226.1824/cursor-chg-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "cursor-chg" "20131226.1824" "Change cursor dynamically, depending on the context." (quote nil))
--- .emacs.d/elpa/f-20140828.716/f-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; f-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("f-pkg.el" "f.el") (21650 58119 52837
-;;;;;;  742000))
-
-;;;***
-
-(provide 'f-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; f-autoloads.el ends here
