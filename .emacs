e7718860625d640d93edbf4d0da15bf6b87bb0c6
Stefan Kangas <stefan.kangas@ericsson.com>
2015-06-30 10:58:07 +0200

    Make w3m configuration optional
    Change-Id: I285d482978cb612f6f3c323e5def5e236f6a9aba

--- .emacs
+++ .emacs
@@ -11,6 +11,29 @@
   `(eval-after-load ,mode
      '(progn ,@body)))
 
+(defvar missing-packages-list nil
+  "List of packages that `try-require' can't find.")
+
+;; attempt to load a feature/library, failing silently
+(defun try-require (feature)
+  "Attempt to load a library or module. Return true if the
+library given as argument is successfully loaded. If not, instead
+of an error, just add the package to a list of missing packages."
+  (condition-case err
+      ;; protected form
+      (progn
+        (message "Checking for library `%s'..." feature)
+        (if (stringp feature)
+            (load-library feature)
+          (require feature))
+        (message "Checking for library `%s'... Found" feature))
+    ;; error handler
+    (file-error  ; condition
+     (progn
+       (message "Checking for library `%s'... Missing" feature)
+       (add-to-list 'missing-packages-list feature 'append))
+     nil)))
+
 (let ((d1 (car  (last (file-expand-wildcards "~/.emacs.d/elpa/auto-compile-20*"))))
       (d2 ".emacs.d/elpa/packed-20130502.2340/"))
   (when (and (file-directory-p d1)
--- .emacs.d/my-w3m.el
+++ .emacs.d/my-w3m.el
@@ -1,115 +1,102 @@
-(require 'w3m-load)
+(try-require 'w3m-load)
 (if (= emacs-major-version 23)
     (progn
-      (require 'w3m)))
+      (try-require 'w3m)))
 
-;(setq w3m-profile-directory "~/.w3m")
+(after 'w3m
+  (setq w3m-home-page "http://www.duckduckgo.com/")
+  (setq w3m-key-binding 'info)
+  (setq w3m-fill-column 80)
+  ;; enable cookies
+  (setq w3m-use-cookies t)
+  (setq w3m-cookie-accept-bad-cookies t)
+  (setq w3m-profile-directory "~/.w3m")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; choose browser
-(setq browse-url-generic-program "firefox")
-(defun choose-browser (url &rest args)
-  (interactive "sURL: ")
-  (if (y-or-n-p "Use external browser? ")
-      (browse-url-generic url)
-    (w3m-browse-url url)))
-;;(setq browse-url-browser-function 'browse-url-generic)
-(setq browse-url-browser-function 'choose-browser)
+  (defun skangas-w3m-bindings ()
+    (define-key w3m-mode-map [?f] 'w3m-isearch-links)
+    ;; (define-key w3m-mode-map "B" 'w3m-view-previous-page)
+    ;; (define-key w3m-mode-map "o" 'w3m-history)
+    ;; (define-key w3m-mode-map "O" 'w3m-db-history)
+    (define-key w3m-mode-map [up] 'previous-line)
+    (define-key w3m-mode-map [down] 'next-line)
+    (define-key w3m-mode-map [left] 'left-char)
+    (define-key w3m-mode-map [right] 'right-char)
+    (define-key w3m-mode-map "h" nil)
+    (define-key w3m-mode-map "j" nil)
+    (define-key w3m-mode-map "k" nil)
+    (define-key w3m-mode-map "l" nil)
+    (define-key w3m-mode-map "W" nil))
+  (add-hook 'w3m-mode-hook 'skangas-w3m-bindings)
 
-(eval-after-load "w3m"
-  '(progn
-     (setq w3m-home-page "http://www.duckduckgo.com/")
-     (setq w3m-key-binding 'info)
-     (setq w3m-fill-column 80)
-     ;; enable cookies
-     (setq w3m-use-cookies t)
-     (setq w3m-cookie-accept-bad-cookies t)
+  (when (require 'w3m-search)
+    (add-to-list 'w3m-search-engine-alist '("DuckDuckGo" "https://duckduckgo.com/lite/?q=%s&kp=1"))
+    (setq w3m-search-default-engine "DuckDuckGo"))
 
-     (defun skangas-w3m-bindings ()
-       (define-key w3m-mode-map [?f] 'w3m-isearch-links)
-       ;; (define-key w3m-mode-map "B" 'w3m-view-previous-page)
-       ;; (define-key w3m-mode-map "o" 'w3m-history)
-       ;; (define-key w3m-mode-map "O" 'w3m-db-history)
-       (define-key w3m-mode-map [up] 'previous-line)
-       (define-key w3m-mode-map [down] 'next-line)
-       (define-key w3m-mode-map [left] 'left-char)
-       (define-key w3m-mode-map [right] 'right-char)
-       (define-key w3m-mode-map "h" nil)
-       (define-key w3m-mode-map "j" nil)
-       (define-key w3m-mode-map "k" nil)
-       (define-key w3m-mode-map "l" nil)
-       (define-key w3m-mode-map "W" nil))
-     (add-hook 'w3m-mode-hook 'skangas-w3m-bindings)
+  ;; descriptive buffer names
+  (add-hook 'w3m-display-hook
+            (lambda (url)
+              (rename-buffer
+               (format "*w3m: %s*" (or w3m-current-title
+                                       w3m-current-url)) t)))
+  ;; remove trailing whitespace
+  (add-hook 'w3m-display-hook
+            (lambda (url)
+              (let ((buffer-read-only nil))
+                (delete-trailing-whitespace))))
 
-     (when (require 'w3m-search)
-       (add-to-list 'w3m-search-engine-alist '("DuckDuckGo" "https://duckduckgo.com/lite/?q=%s&kp=1"))
-       (setq w3m-search-default-engine "DuckDuckGo"))
+  ;; isearch only for links -- prefix arg to search all text
+  (defvar w3m-isearch-links-do-wrap nil
+    "Used internally for fast search wrapping.")
+  (defun w3m-isearch-links (&optional regexp)
+    (interactive "P")
+    (let ((isearch-wrap-function
+           #'(lambda ()
+               (setq w3m-isearch-links-do-wrap nil)
+               (if isearch-forward
+                   (goto-char (window-start))
+                 (goto-char (window-end)))))
+          (isearch-search-fun-function
+           #'(lambda () 'w3m-isearch-links-search-fun))
+          post-command-hook               ;inhibit link echoing
+          do-follow-link
+          (isearch-mode-end-hook
+           (list  #'(lambda nil
+                      (when (and (not isearch-mode-end-hook-quit)
+                                 (w3m-anchor))
+                        (setq do-follow-link t))))))
+      (setq w3m-isearch-links-do-wrap t)
+      (isearch-mode t
+                    regexp
+                    ;; fast wrap
+                    #'(lambda nil
+                        (if isearch-success
+                            (setq w3m-isearch-links-do-wrap t)
+                          (when w3m-isearch-links-do-wrap
+                            (setq w3m-isearch-links-do-wrap nil)
+                            (setq isearch-forward
+                                  (not isearch-forward))
+                            (isearch-repeat isearch-forward))))
+                    t)
+      (when do-follow-link
+        (w3m-view-this-url))))
 
-     ;; descriptive buffer names
-     (add-hook 'w3m-display-hook
-               (lambda (url)
-                 (rename-buffer
-                  (format "*w3m: %s*" (or w3m-current-title
-                                          w3m-current-url)) t)))
-     ;; remove trailing whitespace
-     (add-hook 'w3m-display-hook
-               (lambda (url)
-                 (let ((buffer-read-only nil))
-                   (delete-trailing-whitespace))))
-
-     ;; isearch only for links -- prefix arg to search all text
-     (defvar w3m-isearch-links-do-wrap nil
-       "Used internally for fast search wrapping.")
-     (defun w3m-isearch-links (&optional regexp)
-       (interactive "P")
-       (let ((isearch-wrap-function
-              #'(lambda ()
-                  (setq w3m-isearch-links-do-wrap nil)
-                  (if isearch-forward
-                      (goto-char (window-start))
-                    (goto-char (window-end)))))
-             (isearch-search-fun-function
-              #'(lambda () 'w3m-isearch-links-search-fun))
-             post-command-hook               ;inhibit link echoing
-             do-follow-link
-             (isearch-mode-end-hook
-              (list  #'(lambda nil
-                         (when (and (not isearch-mode-end-hook-quit)
-                                    (w3m-anchor))
-                           (setq do-follow-link t))))))
-         (setq w3m-isearch-links-do-wrap t)
-         (isearch-mode t
-                       regexp
-                       ;; fast wrap
-                       #'(lambda nil
-                           (if isearch-success
-                               (setq w3m-isearch-links-do-wrap t)
-                             (when w3m-isearch-links-do-wrap
-                               (setq w3m-isearch-links-do-wrap nil)
-                               (setq isearch-forward
-                                     (not isearch-forward))
-                               (isearch-repeat isearch-forward))))
-                       t)
-         (when do-follow-link
-           (w3m-view-this-url))))
-
-     (defun w3m-isearch-links-search-fun (string &optional bound no-error)
-       (let* (isearch-search-fun-function
-              (search-fun  (isearch-search-fun))
-              error
-              (bound  (if isearch-forward
-                          (max (or bound 0)
-                               (window-end))
-                        (min (or bound (window-start))
-                             (window-start)))))
-         (condition-case err
-             (while (and (apply search-fun (list string bound))
-                         (not (w3m-anchor (point)))))
-           (error (setq error err)))
-         (if error
-             (if (not no-error)
-                 (signal (car error) (cadr error)))
-           (point))))))
+  (defun w3m-isearch-links-search-fun (string &optional bound no-error)
+    (let* (isearch-search-fun-function
+           (search-fun  (isearch-search-fun))
+           error
+           (bound  (if isearch-forward
+                       (max (or bound 0)
+                            (window-end))
+                     (min (or bound (window-start))
+                          (window-start)))))
+      (condition-case err
+          (while (and (apply search-fun (list string bound))
+                      (not (w3m-anchor (point)))))
+        (error (setq error err)))
+      (if error
+          (if (not no-error)
+              (signal (car error) (cadr error)))
+        (point)))))
 
 (provide 'my-w3m)
 
