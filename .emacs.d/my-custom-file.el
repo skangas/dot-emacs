0b33edcff82aa6b83809b34a8ed42fe1186557f2
Stefan Kangas <skangas@skangas.se>
2016-04-11 18:00:08 +0200

    automatic update @ huey

--- .emacs.d/my-custom-file.el
+++ .emacs.d/my-custom-file.el
@@ -8,8 +8,14 @@
  '(mairix-mail-program (quote gnus))
  '(mairix-search-file "mairix_search")
  '(org-enforce-todo-dependencies t)
- '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-w3m)))
- '(safe-local-variable-values (quote ((system-time-locale . "sv_SE.UTF-8") (truncate-partial-width-windows . t) (epa-file-cache-passphrase-for-symmetric-encryption . t))))
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-w3m)))
+ '(safe-local-variable-values
+   (quote
+    ((system-time-locale . "sv_SE.UTF-8")
+     (truncate-partial-width-windows . t)
+     (epa-file-cache-passphrase-for-symmetric-encryption . t))))
  '(send-mail-function (quote sendmail-send-it)))
 
 (custom-set-faces
@@ -18,4 +24,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(auto-dim-other-buffers-face ((t (:background "#3a3a3a"))))
- '(variable-pitch ((t (:family "droid sans")))))
+ '(variable-pitch ((t (:weight normal :family "edges.se")))))
