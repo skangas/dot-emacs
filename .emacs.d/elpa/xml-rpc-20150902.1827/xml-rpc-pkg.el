dc026e651a5c852d581c84b19f69a8125032ffc7
Stefan Kangas <skangas@skangas.se>
2016-06-13 14:58:03 +0200

    Upgrade elpa packages

--- .emacs.d/elpa/xml-rpc-20150902.1827/xml-rpc-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "xml-rpc" "20150902.1827" "An elisp implementation of clientside XML-RPC" 'nil :url "http://github.com/hexmode/xml-rpc-el" :keywords '("xml" "rpc" "network"))
---
+++ .emacs.d/elpa/xml-rpc-20160430.1458/xml-rpc-pkg.el
@@ -0,0 +1,1 @@
+(define-package "xml-rpc" "20160430.1458" "An elisp implementation of clientside XML-RPC" 'nil :url "http://github.com/hexmode/xml-rpc-el" :keywords '("xml" "rpc" "network"))
