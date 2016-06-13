f4c88631226708978719b81d3bc45692322cf104
Stefan Kangas <skangas@skangas.se>
2016-06-13 14:58:03 +0200

    Upgrade elpa packages

--- .emacs.d/elpa/xml-rpc-20150902.1827/xml-rpc-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; xml-rpc-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("xml-rpc.el") (22280 42193 56590 680000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; xml-rpc-autoloads.el ends here
