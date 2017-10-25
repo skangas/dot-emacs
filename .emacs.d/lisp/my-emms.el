043a2ac2480459db04b0f2c6ed7189586733c641
Stefan Kangas <stefankangas@gmail.com>
2017-10-26 01:54:29 +0200

    update

--- .emacs.d/lisp/my-emms.el
+++ .emacs.d/lisp/my-emms.el
@@ -1,22 +1,23 @@
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp/emms/lisp"))
-(require 'emms-auto)
-(autoload 'emms-browser "emms-setup" "emms-setup" t)
+;; (require 'emms-auto)
+;; (autoload 'emms-browser "emms-setup" "emms-setup" t)
 
 (eval-after-load "emms"
   '(progn
      (require 'emms-setup)
-     (emms-devel)
+     (emms-all)
      (emms-default-players)
 
      (require 'emms-player-mpd)
      (add-to-list 'emms-player-list 'emms-player-mpd)
      (add-to-list 'emms-info-functions 'emms-info-mpd)
-     
-     (setq emms-playlist-buffer-name "*EMMS Playlist*"
-           emms-source-file-default-directory "~/music/"
-           ;; emms-source-file-directory-tree-function 
-           emms-player-mpd-music-directory "~/music/"
-           emms-mode-line-format " %s "
+
+     (setq emms-source-file-default-directory "~/music/")
+     (setq emms-player-mpd-music-directory "~/music/")
+     (setq emms-playlist-buffer-name "*EMMS Playlist*")
+     (setq later-do-interval 0.5)
+
+     ;; emms-source-file-directory-tree-function 
+     (setq emms-mode-line-format " %s "
            emms-show-format "NP: %s"
            emms-player-mpd-server-name "localhost"
            emms-player-mpd-server-port "6600")
@@ -25,15 +26,16 @@
      ;; (global-set-key (kbd "<XF86AudioStop>") 'emms-stop)
      ;; (global-set-key (kbd "<XF86AudioPrev>") 'emms-previous)
      ;; (global-set-key (kbd "<XF86AudioNext>") 'emms-next)
-                                        ;(global-set-key (kbd "<XF86>") 'de-add-dir)
-                                        ;(global-set-key (kbd "<XF86)" 'emms-smart-browse)
-                                        ;(global-set-key (kbd "<XF86ight>") 'emms-seek-forward)
-                                        ;(global-set-key (kbd "<XF86eft>") 'emms-seek-backward)
+     ;;(global-set-key (kbd "<XF86>") 'de-add-dir)
+     ;;(global-set-key (kbd "<XF86)" 'emms-smart-browse)
+     ;;(global-set-key (kbd "<XF86ight>") 'emms-seek-forward)
+     ;;(global-set-key (kbd "<XF86eft>") 'emms-seek-backward)
 
      ;; caching stuff
-     (emms-cache 1)
-     (setq later-do-interval 0.0001
-           emms-info-asynchronously t)))
+     ;; (emms-cache 1)
+     ;; (setq later-do-interval 0.0001
+     ;;       emms-info-asynchronously t)
+     ))
 
 (provide 'my-emms)
 
