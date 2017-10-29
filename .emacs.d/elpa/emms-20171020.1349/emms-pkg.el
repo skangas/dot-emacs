491b60fe11be670fcdfff31c6102818b03c95641
Stefan Kangas <stefankangas@gmail.com>
2017-10-30 00:22:40 +0100

    get rid of unused elpa packages

--- .emacs.d/elpa/emms-20171020.1349/emms-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "emms" "20171020.1349" "The Emacs Multimedia System"
-  '((cl-lib "0.5"))
-  :keywords
-  '("emms" "mp3" "mpeg" "multimedia"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
