2505ba6154807f6bbc9f7be5bd54a440b0281e17
Stefan Kangas <stefankangas@gmail.com>
2017-10-30 02:54:36 +0100

    Upgrade elpa packages

--- .emacs.d/elpa/nameless-1.0.2/nameless-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "nameless" "1.0.2" "Hide package namespace in your emacs-lisp code" '((emacs "24.4")) :url "https://github.com/Malabarba/nameless" :keywords '("convenience" "lisp"))
---
+++ .emacs.d/elpa/packed-20170819.942/packed-autoloads.el
@@ -0,0 +1,15 @@
+;;; packed-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("packed.el") (23030 34749 579419 552000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; packed-autoloads.el ends here
