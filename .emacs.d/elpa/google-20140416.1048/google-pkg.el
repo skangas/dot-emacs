84cc5768bf22ebd4de3d1907139af650018b1938
Stefan Kangas <stefankangas@gmail.com>
2017-10-30 02:19:33 +0100

    Revert "get rid of unused elpa packages"
    This reverts commit 83bc68b4e6b6ae65e0924c5a60eea20a58eff81c.

---
+++ .emacs.d/elpa/google-20140416.1048/google-pkg.el
@@ -0,0 +1,1 @@
+(define-package "google" "20140416.1048" "Emacs interface to the Google API" 'nil :keywords '("comm" "processes" "tools"))
---
+++ .emacs.d/elpa/redshank-20120510.1230/redshank-pkg.el
@@ -0,0 +1,2 @@
+(define-package "redshank" "20120510.1230" "No description available. [git]" 'nil)
+
