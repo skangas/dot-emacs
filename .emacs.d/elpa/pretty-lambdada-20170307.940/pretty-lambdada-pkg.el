442752679c21c4145424c385fc5780f2df5caa8a
Stefan Kangas <stefankangas@gmail.com>
2017-10-30 02:19:50 +0100

    Revert "update packages"
    This reverts commit b0b461dd9b825f9facf827a1b19120e55ca34be3.

--- .emacs.d/elpa/pretty-lambdada-20170307.940/pretty-lambdada-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "pretty-lambdada" "20170307.940" "Show the word `lambda' as the Greek letter." 'nil :url "https://www.emacswiki.org/emacs/download/pretty-lambdada.el" :keywords '("convenience" "display"))
