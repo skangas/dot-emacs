ac8c50c37ed6a1d190021989fae51c639c0a0fc1
Stefan Kangas <stefankangas@gmail.com>
2017-11-10 03:00:06 +0100

    automatic update @ joffe

--- .emacs.d/lisp/my-org-mode.el
+++ .emacs.d/lisp/my-org-mode.el
@@ -549,6 +549,32 @@ Skip project and sub-project tasks, habits, and loose non-project tasks."
    "Is the task at POM or point a habit?"
    (string= "habit" (org-entry-get (or pom (point)) "STYLE"))))
 
+(setq org-archive-mark-done nil)
+(setq org-archive-location "%s_archive::* Archived Tasks")
+
+(defun bh/skip-non-archivable-tasks ()
+  "Skip trees that are not available for archiving"
+  (save-restriction
+    (widen)
+    ;; Consider only tasks with done todo headings as archivable candidates
+    (let ((next-headline (save-excursion (or (outline-next-heading) (point-max))))
+          (subtree-end (save-excursion (org-end-of-subtree t))))
+      (if (member (org-get-todo-state) org-todo-keywords-1)
+          (if (member (org-get-todo-state) org-done-keywords)
+              (let* ((daynr (string-to-int (format-time-string "%d" (current-time))))
+                     (a-month-ago (* 60 60 24 (+ daynr 1)))
+                     (last-month (format-time-string "%Y-%m-" (time-subtract (current-time) (seconds-to-time a-month-ago))))
+                     (this-month (format-time-string "%Y-%m-" (current-time)))
+                     (subtree-is-current (save-excursion
+                                           (forward-line 1)
+                                           (and (< (point) subtree-end)
+                                                (re-search-forward (concat last-month "\\|" this-month) subtree-end t)))))
+                (if subtree-is-current
+                    subtree-end ; Has a date in this month or last month, skip it
+                  nil))  ; available to archive
+            (or subtree-end (point-max)))
+        next-headline))))
+
 (provide 'my-org-mode)
 
 ;; my-org-mode.el ends here
