e9204b19db999a9248d3d0cb670b8352b0bb8729
Stefan Kangas <stefankangas@gmail.com>
2018-03-05 09:38:46 +0100

    automatic update @ joffe

--- .emacs.d/lisp/my-keybindings.el
+++ .emacs.d/lisp/my-keybindings.el
@@ -135,6 +135,17 @@ user."
       (setq file (concat "/sudo:root@localhost:" file)))
     (find-file file)))
 
+;;; Copy line
+(defun copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring"
+  (interactive "p")
+  (kill-ring-save (line-beginning-position)
+                  (line-beginning-position (+ 1 arg)))
+  (message "%d line%s copied" arg (if (= 1 arg) "" "s")))
+
+; optional key binding
+(global-set-key "\C-c\C-k" 'copy-line)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (global-set-key (kbd "C-x g") 'magit-status)
