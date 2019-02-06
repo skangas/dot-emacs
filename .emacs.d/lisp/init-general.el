2f39447e160d019dc32381c094c87fa87f6361e3
Stefan Kangas <stefankangas@gmail.com>
2019-02-06 12:15:02 +0100

    automatic update @ macbook

--- .emacs.d/lisp/init-general.el
+++ .emacs.d/lisp/init-general.el
@@ -355,6 +355,27 @@
     (let ((cmd "feh -F -Z * &" ))
       (message cmd)
       (dired-do-shell-command cmd nil (list (dired-get-file-for-visit)))))
+
+  ;; (defun diredext-exec-git-command-in-shell (command &optional arg file-list)
+;;   "Run a shell command 
+;; git COMMAND
+;; ' on the marked files.
+;; if no files marked, always operate on current line in dired-mode
+;; "
+;;   (interactive
+;;    (let ((files (dired-get-marked-files t current-prefix-arg)))
+;;      (list
+;;       ;; Want to give feedback whether this file or marked files are used:
+;;       (dired-read-shell-command "git command on %s: " current-prefix-arg files)
+;;       current-prefix-arg
+;;       files)))
+;;   (unless (string-match "[?][ \t]\'" command)
+;;     (setq command (concat command " *")))
+;;   (setq command (concat "git " command))
+;;   (dired-do-shell-command command arg file-list)
+;;   (message command))
+  ;; (eval-after-load 'dired '(define-key dired-mode-map "/" 'diredext-exec-git-command-in-shell))
+  
   (add-hook 'dired-mode-hook 'dired-hide-details-mode))
 
 (use-package discover
