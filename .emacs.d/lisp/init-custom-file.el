d3c46bc53a8ab286c98215d606a33e8deed6466b
Stefan Kangas <stefankangas@gmail.com>
2019-06-09 10:45:01 +0200

    automatic update @ macbook

--- .emacs.d/lisp/init-custom-file.el
+++ .emacs.d/lisp/init-custom-file.el
@@ -3,23 +3,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bfdcbf0d33f3376a956707e746d10f3ef2d8d9caa1c214361c9c08f00a1c8409" default))
  '(ecb-options-version "2.32")
  '(mairix-file-path "~/Maildir/")
- '(mairix-mail-program (quote gnus))
+ '(mairix-mail-program 'gnus)
  '(mairix-search-file "mairix_search")
  '(org-enforce-todo-dependencies t)
  '(org-modules
-   (quote
-    (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-w3m)))
+   '(org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-w3m))
  '(package-selected-packages
-   (quote
-    (debbugs sublimity org-download org-ac org-bullets org-journal go-mode diff-hl notmuch hydra multiple-cursors iedit rinari smartparens rvm ruby-test-mode robe projectile-rails projectile enh-ruby-mode pymacs macrostep jde auto-package-update auto-complete openwith open-with ido-completing-read+ inf-ruby elisp-slime-nav minitest eruby-mode centered-window xml-rpc haskell-mode google-translate f exec-path-from-shell discover cursor-chg centered-cursor-mode boxquote auto-dim-other-buffers magit dashboard geiser zenburn-theme yaml-mode window-numbering use-package smex paredit nameless elfeed-org auto-compile async)))
+   '(debbugs sublimity org-download org-ac org-bullets org-journal go-mode diff-hl notmuch hydra multiple-cursors iedit rinari smartparens rvm ruby-test-mode robe projectile-rails projectile enh-ruby-mode pymacs macrostep jde auto-package-update auto-complete openwith open-with ido-completing-read+ inf-ruby elisp-slime-nav minitest eruby-mode centered-window xml-rpc haskell-mode google-translate f exec-path-from-shell discover cursor-chg centered-cursor-mode boxquote auto-dim-other-buffers magit dashboard geiser zenburn-theme yaml-mode window-numbering use-package smex paredit nameless elfeed-org auto-compile async))
  '(safe-local-variable-values
-   (quote
-    ((system-time-locale . "sv_SE.UTF-8")
+   '((system-time-locale . "sv_SE.UTF-8")
      (truncate-partial-width-windows . t)
-     (epa-file-cache-passphrase-for-symmetric-encryption . t))))
- '(send-mail-function (quote sendmail-send-it)))
+     (epa-file-cache-passphrase-for-symmetric-encryption . t)))
+ '(send-mail-function 'sendmail-send-it))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
