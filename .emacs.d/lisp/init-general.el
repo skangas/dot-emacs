7a83ef3b023db95740276983895fc9956665b696
Stefan Kangas <stefankangas@gmail.com>
2019-10-09 00:00:15 +0200

    automatic update @ joffe

--- .emacs.d/lisp/init-general.el
+++ .emacs.d/lisp/init-general.el
@@ -685,16 +685,17 @@
   ;; Replaces window-numbering.el
   :ensure t
   :pin "melpa-stable"
-  :bind (("M-1" . winum-select-window-1)
-         ("M-2" . winum-select-window-2)
-         ("M-3" . winum-select-window-3)
-         ("M-4" . winum-select-window-4)
-         ("M-5" . winum-select-window-5)
-         ("M-6" . winum-select-window-6)
-         ("M-7" . winum-select-window-7)
-         ("M-8" . winum-select-window-8)
-         ("M-9" . winum-select-window-9))
+  ;; :bind (("M-1" . winum-select-window-1)
+  ;;        ("M-2" . winum-select-window-2)
+  ;;        ("M-3" . winum-select-window-3)
+  ;;        ("M-4" . winum-select-window-4)
+  ;;        ("M-5" . winum-select-window-5)
+  ;;        ("M-6" . winum-select-window-6)
+  ;;        ("M-7" . winum-select-window-7)
+  ;;        ("M-8" . winum-select-window-8)
+  ;;        ("M-9" . winum-select-window-9))
   :config
+  (setq winum-scope 'frame-local)
   (winum-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
