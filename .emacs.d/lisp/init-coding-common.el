3221a981a6eb1547f84d213da40ba117d01088d6
Stefan Kangas <stefankangas@gmail.com>
2020-01-22 09:00:04 +0100

    automatic update @ joffe

--- .emacs.d/lisp/init-coding-common.el
+++ .emacs.d/lisp/init-coding-common.el
@@ -84,6 +84,50 @@
   :config
   (add-hook 'enh-ruby-mode-hook 'smartparens-mode))
 
+(use-package smerge-mode
+  :after hydra
+
+  ;; This hydra taken from
+  ;; https://github.com/alphapapa/unpackaged.el#hydra
+  :config
+  (defhydra unpackaged/smerge-hydra
+    (:color pink :hint nil :post (smerge-auto-leave))
+    "
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
+"
+    ("n" smerge-next)
+    ("p" smerge-prev)
+    ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
+    ("a" smerge-keep-all)
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("R" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("k" smerge-kill-current)
+    ("ZZ" (lambda ()
+            (interactive)
+            (save-buffer)
+            (bury-buffer))
+     "Save and bury buffer" :color blue)
+    ("q" nil "cancel" :color blue))
+  :hook (magit-diff-visit-file . (lambda ()
+                                   (when smerge-mode
+                                     (unpackaged/smerge-hydra/body)
+                                     (recenter-top-bottom)))))
+
 (use-package web-mode
   :mode (("\\.js\\'" . web-mode)
          ("\\.jsx\\'" . web-mode)
