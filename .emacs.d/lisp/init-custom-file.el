126422d193f464283312ec8c99da7beba607194a
Stefan Kangas <stefankangas@gmail.com>
2020-02-01 15:45:01 +0100

    automatic update @ macbook

--- .emacs.d/lisp/init-custom-file.el
+++ .emacs.d/lisp/init-custom-file.el
@@ -21,9 +21,6 @@
  '(org-enforce-todo-dependencies t)
  '(org-modules
    '(org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-w3m))
- '(package-archives
-   '(("gnu" . "http://elpa.gnu.org/packages/")
-     ("ung" . "http://elpa.gnu.org/")))
  '(package-selected-packages
    '(org-babel-eval-in-repl guess-language-mode guess-language ivy browse-kill-ring winum bonjourmadame org-plus-contrib debbugs sublimity org-download org-ac org-bullets org-journal go-mode diff-hl notmuch hydra multiple-cursors iedit rinari smartparens rvm ruby-test-mode robe projectile-rails projectile enh-ruby-mode pymacs macrostep jde auto-package-update auto-complete openwith open-with ido-completing-read+ inf-ruby elisp-slime-nav minitest eruby-mode centered-window xml-rpc haskell-mode google-translate f exec-path-from-shell discover cursor-chg centered-cursor-mode boxquote auto-dim-other-buffers magit dashboard geiser zenburn-theme yaml-mode window-numbering use-package smex paredit nameless elfeed-org auto-compile async))
  '(safe-local-variable-values
