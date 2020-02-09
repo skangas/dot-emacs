44f234638f059950c56c709059702c23de6ec40a
Stefan Kangas <stefankangas@gmail.com>
2020-02-10 00:00:17 +0100

    automatic update @ joffe

--- custom-file.el
+++ custom-file.el
@@ -24,6 +24,7 @@
  '(package-archives
    '(("gnu" . "http://elpa.gnu.org/packages/")
      ("ung" . "http://elpa.gnu.org/")))
+ '(package-hidden-regexps '("\\`0blayout\\'" "\\`amx\\'"))
  '(package-selected-packages
    '(anki-editor org-babel-eval-in-repl guess-language-mode guess-language ivy browse-kill-ring winum bonjourmadame org-plus-contrib debbugs sublimity org-download org-ac org-bullets org-journal go-mode diff-hl notmuch hydra multiple-cursors iedit rinari smartparens rvm ruby-test-mode robe projectile-rails projectile enh-ruby-mode pymacs macrostep jde auto-package-update auto-complete openwith open-with ido-completing-read+ inf-ruby elisp-slime-nav minitest eruby-mode centered-window xml-rpc haskell-mode google-translate f exec-path-from-shell discover cursor-chg centered-cursor-mode boxquote auto-dim-other-buffers magit dashboard geiser zenburn-theme yaml-mode window-numbering use-package smex paredit nameless elfeed-org auto-compile async))
  '(safe-local-variable-values
