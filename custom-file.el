1f946efcb87f047cf3ab3f62d0ce019ed569dc3d
Stefan Kangas <stefankangas@gmail.com>
2020-08-19 00:00:13 +0200

    automatic update @ joffe

--- custom-file.el
+++ custom-file.el
@@ -3,39 +3,31 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#ebdbb2" "#9d0006" "#98971a" "#b57614" "#076678" "#d3869b" "#689d6a" "#3c3836"])
  '(auth-source-save-behavior nil)
  '(company-quickhelp-color-background "#4F4F4F")
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(compilation-message-face 'default)
- '(cua-global-mark-cursor-color "#689d6a")
- '(cua-normal-cursor-color "#7c6f64")
- '(cua-overwrite-cursor-color "#b57614")
- '(cua-read-only-cursor-color "#98971a")
+ '(cua-global-mark-cursor-color "#11948b")
+ '(cua-normal-cursor-color "#596e76")
+ '(cua-overwrite-cursor-color "#a67c00")
+ '(cua-read-only-cursor-color "#778c00")
  '(custom-safe-themes
    '("285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "0cb1b0ea66b145ad9b9e34c850ea8e842c4c4c83abe04e37455a1ef4cc5b8791" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "a06658a45f043cd95549d6845454ad1c1d6e24a99271676ae56157619952394a" "84890723510d225c45aaff941a7e201606a48b973f0121cb9bcb0b9399be8cba" "bfdcbf0d33f3376a956707e746d10f3ef2d8d9caa1c214361c9c08f00a1c8409" default))
  '(ecb-options-version "2.32")
- '(fci-rule-color "#ebdbb2")
- '(highlight-changes-colors '("#d3869b" "#8f3f71"))
+ '(fci-rule-color "#405A61")
+ '(highlight-changes-colors '("#c42475" "#5e65b6"))
  '(highlight-symbol-colors
-   '("#ed94d1d39b5c" "#d6a5dca3af86" "#eb90bc25933d" "#e1bec426b1e4" "#e40dda889de7" "#ef28c40e9555" "#c667cd42b3b9"))
- '(highlight-symbol-foreground-color "#665c54")
- '(highlight-tail-colors
-   '(("#ebdbb2" . 0)
-     ("#c6c148" . 20)
-     ("#82cc73" . 30)
-     ("#5b919b" . 50)
-     ("#e29a3f" . 60)
-     ("#df6835" . 70)
-     ("#f598a7" . 85)
-     ("#ebdbb2" . 100)))
+   '("#ed7ddb24b29e" "#cd82e29fd17c" "#fc9acadfb443" "#d974d4beddd6" "#df07dfc6b349" "#f76ccd6eaf2a" "#d132db91e15a"))
+ '(highlight-symbol-foreground-color "#5d737a")
  '(hl-bg-colors
-   '("#e29a3f" "#df6835" "#cf5130" "#f598a7" "#c2608f" "#5b919b" "#82cc73" "#c6c148"))
+   '("#d6a549" "#ed6e3e" "#ff6243" "#f46495" "#837bdf" "#6fa5e7" "#66c1b3" "#a8b84b"))
  '(hl-fg-colors
-   '("#fbf1c7" "#fbf1c7" "#fbf1c7" "#fbf1c7" "#fbf1c7" "#fbf1c7" "#fbf1c7" "#fbf1c7"))
- '(hl-paren-colors '("#689d6a" "#b57614" "#076678" "#8f3f71" "#98971a"))
- '(lsp-ui-doc-border "#665c54")
+   '("#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9"))
+ '(hl-paren-colors '("#11948b" "#a67c00" "#007ec4" "#5e65b6" "#778c00"))
+ '(jdee-db-active-breakpoint-face-colors (cons "#073642" "#268bd2"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#073642" "#859900"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#073642" "#56697A"))
+ '(lsp-ui-doc-border "#5d737a")
  '(mairix-file-path "~/Maildir/")
  '(mairix-mail-program 'gnus)
  '(mairix-search-file "mairix_search")
@@ -53,52 +45,30 @@
      (:name "emacs" :query "(tag:emacs-devel OR tag:emacs-bugs OR tag:emacs-diffs OR tag:emacs-help) AND tag:unread" :key "e")))
  '(nrepl-message-colors
    '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
+ '(objed-cursor-color "#dc322f")
  '(org-enforce-todo-dependencies t)
  '(org-modules
    '(org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-habit org-irc org-w3m))
  '(package-selected-packages
-   '(ob-async solarized-theme anki-editor org-babel-eval-in-repl guess-language-mode guess-language ivy browse-kill-ring winum bonjourmadame org-plus-contrib debbugs sublimity org-download org-ac org-bullets org-journal go-mode diff-hl notmuch hydra multiple-cursors iedit rinari smartparens rvm ruby-test-mode robe projectile-rails projectile enh-ruby-mode pymacs macrostep jde auto-package-update auto-complete openwith open-with ido-completing-read+ inf-ruby minitest eruby-mode centered-window xml-rpc haskell-mode google-translate f exec-path-from-shell discover cursor-chg centered-cursor-mode boxquote auto-dim-other-buffers magit dashboard geiser zenburn-theme yaml-mode window-numbering use-package smex paredit nameless elfeed-org auto-compile async))
+   '(modus-vivendi-theme modus-operandi-theme ob-async solarized-theme anki-editor org-babel-eval-in-repl guess-language-mode guess-language ivy browse-kill-ring winum bonjourmadame org-plus-contrib debbugs sublimity org-download org-ac org-bullets org-journal go-mode diff-hl notmuch hydra multiple-cursors iedit rinari smartparens rvm ruby-test-mode robe projectile-rails projectile enh-ruby-mode pymacs macrostep jde auto-package-update auto-complete openwith open-with ido-completing-read+ inf-ruby minitest eruby-mode centered-window xml-rpc haskell-mode google-translate f exec-path-from-shell discover cursor-chg centered-cursor-mode boxquote auto-dim-other-buffers magit dashboard geiser zenburn-theme yaml-mode window-numbering use-package smex paredit nameless elfeed-org auto-compile async))
  '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
- '(pos-tip-background-color "#ebdbb2")
- '(pos-tip-foreground-color "#665c54")
+ '(pos-tip-background-color "#f4eedb")
+ '(pos-tip-foreground-color "#5d737a")
+ '(rustic-ansi-faces
+   ["#002b36" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#839496"])
  '(safe-local-variable-values
    '((eval add-hook 'before-save-hook #'copyright-update nil t)
      (system-time-locale . "sv_SE.UTF-8")
      (truncate-partial-width-windows . t)
      (epa-file-cache-passphrase-for-symmetric-encryption . t)))
+ '(savehist-mode t)
  '(send-mail-function 'sendmail-send-it)
- '(smartrep-mode-line-active-bg (solarized-color-blend "#98971a" "#ebdbb2" 0.2))
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#778c00" "#f4eedb" 0.2))
  '(smerge-command-prefix "v")
- '(term-default-bg-color "#fbf1c7")
- '(term-default-fg-color "#7c6f64")
- '(vc-annotate-background nil)
- '(vc-annotate-background-mode nil)
- '(vc-annotate-color-map
-   '((20 . "#9d0006")
-     (40 . "#ab324a550b62")
-     (60 . "#b0da61140f75")
-     (80 . "#b57614")
-     (100 . "#ad01822d15d7")
-     (120 . "#a85287bf16d2")
-     (140 . "#a3608d2917dc")
-     (160 . "#9e26926f18f4")
-     (180 . "#98971a")
-     (200 . "#8bd699a03aed")
-     (220 . "#84849aa247bf")
-     (240 . "#7c5b9ba153ba")
-     (260 . "#731d9c9f5f38")
-     (280 . "#689d6a")
-     (300 . "#4df186d970f7")
-     (320 . "#3e5d7bc873bc")
-     (340 . "#2af870f57639")
-     (360 . "#076678")))
- '(vc-annotate-very-old-color nil)
+ '(term-default-bg-color "#fffce9")
+ '(term-default-fg-color "#596e76")
  '(weechat-color-list
-   '(unspecified "#fbf1c7" "#ebdbb2" "#750000" "#9d0006" "#747400" "#98971a" "#8a5100" "#b57614" "#004858" "#076678" "#9f4d64" "#d3869b" "#2e7d33" "#689d6a" "#7c6f64" "#3c3836"))
- '(xterm-color-names
-   ["#ebdbb2" "#9d0006" "#98971a" "#b57614" "#076678" "#d3869b" "#689d6a" "#32302f"])
- '(xterm-color-names-bright
-   ["#fbf1c7" "#af3a03" "#a89984" "#3c3836" "#7c6f64" "#8f3f71" "#665c54" "#282828"]))
+   '(unspecified "#fffce9" "#f4eedb" "#990001" "#cc1f24" "#4f6600" "#778c00" "#785700" "#a67c00" "#005797" "#007ec4" "#93004d" "#c42475" "#006d68" "#11948b" "#596e76" "#88999b")))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
