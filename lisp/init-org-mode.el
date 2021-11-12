eb814930840a0f056e09aa353f473769c04e5b45
Stefan Kangas <stefan@marxist.se>
2021-11-12 11:05:16 +0100

    Disable org-roam for now

--- lisp/init-org-mode.el
+++ lisp/init-org-mode.el
@@ -903,5 +903,18 @@ so change the default 'F' binding in the agenda to allow both"
           '(lambda () (org-defkey org-agenda-mode-map "V" 'bh/view-next-project))
           'append)
 
+
+;; (use-package org-roam
+;;   :ensure t
+;;   :hook
+;;   (after-init . org-roam-mode)
+;;   :bind (:map org-roam-mode-map
+;;               (("C-c n l" . org-roam)
+;;                ("C-c n f" . org-roam-find-file)
+;;                ("C-c n g" . org-roam-graph))
+;;               :map org-mode-map
+;;               (("C-c n i" . org-roam-insert))
+;;               (("C-c n I" . org-roam-insert-immediate))))
+
 (provide 'init-org-mode)
 ;; init-org-mode.el ends here
