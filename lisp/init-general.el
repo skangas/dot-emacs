bb607ab09bb13c9852b312479bc2793390251a8f
Stefan Kangas <stefan@marxist.se>
2022-06-21 02:15:55 +0200

    Add embark package

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -419,6 +419,25 @@
   :config
   (global-discover-mode 1))
 
+(use-package embark
+  :ensure t
+  :pin "gnu"
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
 (use-package epa-file
   :config
   ;;(setq epa-armor t)
