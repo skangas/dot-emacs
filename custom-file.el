b311b1c2ef9b4e9185ef12ac7de6cf5fe006ec58
Stefan Kangas <stefankangas@gmail.com>
2022-12-09 05:20:37 +0100

    Update custom-file.el

--- custom-file.el
+++ custom-file.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Man-switches "--no-hyphenation --no-justification")
  '(Man-width-max 76)
  '(abbrev-suggest t)
  '(ansi-color-faces-vector
@@ -21,12 +22,13 @@
  '(company-quickhelp-color-foreground "#DCDCCC")
  '(company-tooltip-limit 15)
  '(compilation-message-face 'default)
+ '(compilation-scroll-output t)
  '(confirm-kill-emacs 'y-or-n-p)
  '(copyright-year-ranges t)
  '(cua-enable-cua-keys nil)
  '(cua-global-mark-cursor-color "#689d6a")
- '(cua-normal-cursor-color "#7c6f64")
- '(cua-overwrite-cursor-color "#b57614")
+ '(cua-normal-cursor-color "#a89984")
+ '(cua-overwrite-cursor-color "#d79921")
  '(cua-read-only-cursor-color "#98971a")
  '(custom-safe-themes
    '("56ed144b399e3fbf1fcfc5af854f0053b21c0e3e7cfc824f0473da6f4e179695" "4f1d2476c290eaa5d9ab9d13b60f2c0f1c8fa7703596fa91b235db7f99a9441b" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "f6665ce2f7f56c5ed5d91ed5e7f6acb66ce44d0ef4acfaa3a42c7cfe9e9a9013" "0466adb5554ea3055d0353d363832446cd8be7b799c39839f387abb631ea0995" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" "0fffa9669425ff140ff2ae8568c7719705ef33b7a927a0ba7c5e2ffcfac09b75" "01cf34eca93938925143f402c2e6141f03abb341f27d1c2dba3d50af9357ce70" "0fe24de6d37ea5a7724c56f0bb01efcbb3fe999a6e461ec1392f3c3b105cc5ac" "d6603a129c32b716b3d3541fc0b6bfe83d0e07f1954ee64517aa62c9405a3441" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "8f5a7a9a3c510ef9cbb88e600c0b4c53cdcdb502cfe3eb50040b7e13c6f4e78e" "9196767d9aed967c1621fe43372860d9b219dbca4b0c6adb07603499c18fa79b" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "0cb1b0ea66b145ad9b9e34c850ea8e842c4c4c83abe04e37455a1ef4cc5b8791" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "a06658a45f043cd95549d6845454ad1c1d6e24a99271676ae56157619952394a" "84890723510d225c45aaff941a7e201606a48b973f0121cb9bcb0b9399be8cba" "bfdcbf0d33f3376a956707e746d10f3ef2d8d9caa1c214361c9c08f00a1c8409" default))
@@ -143,6 +145,7 @@
  '(lsp-ui-doc-border "#665c54")
  '(magit-diff-use-overlays nil)
  '(magit-log-section-commit-count 12)
+ '(magit-openpgp-default-signing-key "CEA1DE21AB108493CC9C65742E82323B8F4353EE")
  '(mairix-file-path "~/Maildir/")
  '(mairix-mail-program 'gnus)
  '(mairix-search-file "mairix_search")
@@ -159,20 +162,32 @@
  '(modus-themes-syntax '(green-strings yellow-comments))
  '(modus-themes-variable-pitch-ui t)
  '(mouse-highlight 1)
+ '(mpc-mpd-music-directory "~/music")
  '(native-comp-async-report-warnings-errors nil)
  '(notmuch-saved-searches
-   '((:name "inbox" :query "tag:inbox" :key "i")
-     (:name "unread" :query "tag:unread" :key "u")
-     (:name "flagged" :query "tag:flagged" :key "f")
-     (:name "sent" :query "tag:sent" :key "t")
-     (:name "drafts" :query "tag:draft" :key "d")
-     (:name "all mail" :query "*" :key "a")
+   '((:name "inbox" :query "tag:inbox" :key
+            [105])
+     (:name "unread" :query "tag:unread" :key
+            [117])
+     (:name "flagged" :query "tag:flagged" :key
+            [102])
+     (:name "sent" :query "tag:sent" :key
+            [116])
+     (:name "drafts" :query "tag:draft" :key
+            [100])
+     (:name "all mail" :query "*" :key
+            [97])
      (:name "ek" :query "tag:ek")
      (:name "desk" :query "tag:desk AND tag:unread")
      (:name "logcheck" :query "tag:logcheck AND not tag:trash AND not tag:deleted")
      (:name "emacs::bugs" :query "tag:emacs::bugs AND tag:unread")
-     (:name "emacs" :query "((tag:emacs-devel OR tag:emacs-bugs OR tag:emacs-diffs OR tag:emacs-help OR tag:emacs-todo OR tag:emacs-elpa-diffs) AND tag:unread) AND not tag:muted" :key "e")
-     (:name "emacs-all" :query "(((tag:emacs-devel OR tag:emacs-bugs OR tag:emacs-diffs OR tag:emacs-help OR tag:emacs-elpa-diffs) AND tag:unread) OR tag:emacs-todo) AND not tag:muted" :key "f")))
+     (:name "emacs" :query "((tag:emacs-devel OR tag:emacs-bugs OR tag:emacs-diffs OR tag:emacs-help OR tag:emacs-todo OR tag:emacs-elpa-diffs) AND tag:unread) AND not tag:muted" :key
+            [101])
+     (:name "emacs-all" :query "(((tag:emacs-devel OR tag:emacs-bugs OR tag:emacs-diffs OR tag:emacs-help OR tag:emacs-elpa-diffs) AND tag:unread) OR tag:emacs-todo) AND not tag:muted" :key
+            [102])
+     (:name "debian" :query "tag:debian-mentors and tag:unread" :key
+            [104])))
+ '(notmuch-search-oldest-first nil)
  '(nov-text-width 70)
  '(nrepl-message-colors
    '("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3"))
@@ -185,7 +200,7 @@
  '(outline-minor-mode-cycle t)
  '(package-quickstart t)
  '(package-selected-packages
-   '(picpocket nov xml-rpc gruvbox-theme gotham-theme engine-mode svg-tag-mode org-modern-mode org-modern orglink w3m haskell-mode bbdb geiser minitest ruby-test-mode robe projectile-rails enh-ruby-mode prescient selectrum ioccur consult elsearch xr aggressive-indent doom-themes marginalia el-search monokai-theme color-theme-sanityinc-tomorrow solarized-theme php-mode suggest macrostep nameless yaml-mode yasnippet-snippets yasnippet ws-butler web-mode smartparens projectile paredit markdown-mode winum which-key wgrep visual-fill-column use-package undo-tree org-journal org-download org-bullets openwith ob-async multiple-cursors magit iedit hydra flx f elfeed-org discover diminish diff-hl debbugs counsel company centered-window browse-kill-ring boxquote auto-compile ag))
+   '(writegood-mode yaml-mode yasnippet-snippets yasnippet xr ws-butler web-mode projectile php-mode paredit package-lint nameless magit macrostep haskell-mode el-search engine-mode orderless ag ob-async markdown-mode diff-hl winum company undo-tree hydra org-download openwith use-package wgrep xml-rpc org-modern org-super-agenda which-key ioccur nov diminish centered-window counsel orglink boxquote aggressive-indent visual-fill-column debbugs marginalia multiple-cursors avy iedit embark elfeed-org))
  '(pdf-view-bounding-box-margin 0.15)
  '(pdf-view-midnight-colors '("#DCDCCC" . "#383838"))
  '(pdf-view-resize-factor 1.05)
@@ -220,6 +235,8 @@
  '(term-default-fg-color "#7c6f64")
  '(tool-bar-position 'top)
  '(uniquify-buffer-name-style 'forward nil (uniquify))
+ '(use-package-always-ensure t)
+ '(use-package-enable-imenu-support t)
  '(use-short-answers t)
  '(vc-annotate-background-mode nil)
  '(visible-bell t)
