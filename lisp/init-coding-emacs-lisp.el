89a57ca77d0db2597b566c5e56015663318da813
Stefan Kangas <stefankangas@gmail.com>
2023-06-06 12:56:24 +0200

    Don't recompile my init files on macOS

--- lisp/init-coding-emacs-lisp.el
+++ lisp/init-coding-emacs-lisp.el
@@ -9,17 +9,18 @@
 
 (defun my-recompile-el ()
   (interactive)
-  (when (and buffer-file-name
-             (not (string-match "^/home/skangas/wip/emacs" buffer-file-name))
-             (not (string-match "^/home/skangas/.emacs.d" buffer-file-name))
-             (not (string-match "\\.dir-locals\\.el$"  buffer-file-name))
-             (string-match "/.*\\.el$"  buffer-file-name)
-                                        ;                     (string-match init-file-user buffer-file-name)) ;; XXX: doesn't work
-             (file-newer-than-file-p buffer-file-name
-                                     (concat buffer-file-name "c")))
-    (when (file-exists-p (concat buffer-file-name ".elc"))
-      (delete-file (concat buffer-file-name ".elc")))
-    (byte-compile-file buffer-file-name)))
+  (rx-let ((home (: bos "/" (or "home" "Users") "/skangas")))
+      (when (and buffer-file-name
+                 (not (string-match (rx home "/wip/emacs") buffer-file-name))
+                 (not (string-match (rx home "/.emacs.d") buffer-file-name))
+                 (not (string-match "\\.dir-locals\\.el$"  buffer-file-name))
+                 (string-match "/.*\\.el$"  buffer-file-name)
+                 ;; (string-match init-file-user buffer-file-name)) ;; XXX: doesn't work
+                 (file-newer-than-file-p buffer-file-name
+                                         (concat buffer-file-name "c")))
+        (when (file-exists-p (concat buffer-file-name ".elc"))
+          (delete-file (concat buffer-file-name ".elc")))
+        (byte-compile-file buffer-file-name))))
 
 (defun my-emacs-lisp-mode-hook ()
   (my-coding-keys emacs-lisp-mode-map)
