162be1e73aa3ff2f382063749c9dc454eb5ef089
Stefan Kangas <stefankangas@gmail.com>
2023-07-19 07:43:12 +0200

    Run check-parens only in ELisp buffers

--- lisp/init-coding-emacs-lisp.el
+++ lisp/init-coding-emacs-lisp.el
@@ -5,25 +5,25 @@
 ;; (require 'eval-expr)
 ;; (eval-expr-install)
 
-(defun sk/emacs-lisp-data-mode-hook ()
-  (add-hook 'write-file-functions #'check-parens))
-(add-hook 'lisp-data-mode-hook #'sk/emacs-lisp-data-mode-hook)
+(add-hook 'lisp-data-mode-hook
+          (lambda ()
+            (add-hook 'write-file-functions #'check-parens nil t)))
 
 (defun my-recompile-el ()
   "Recompile Emacs Lisp files automatically, but not in some directories."
   (interactive)
   (rx-let ((home (: bos "/" (or "home" "Users") "/skangas")))
-      (when (and buffer-file-name
-                 (not (string-match (rx home "/wip/emacs") buffer-file-name))
-                 (not (string-match (rx home "/.emacs.d") buffer-file-name))
-                 (not (string-match "\\.dir-locals\\.el$"  buffer-file-name))
-                 (string-match "/.*\\.el$"  buffer-file-name)
-                 ;; (string-match init-file-user buffer-file-name)) ;; XXX: doesn't work
-                 (file-newer-than-file-p buffer-file-name
-                                         (concat buffer-file-name "c")))
-        (when (file-exists-p (concat buffer-file-name ".elc"))
-          (delete-file (concat buffer-file-name ".elc")))
-        (byte-compile-file buffer-file-name))))
+    (when (and buffer-file-name
+               (not (string-match (rx home "/wip/emacs") buffer-file-name))
+               (not (string-match (rx home "/.emacs.d") buffer-file-name))
+               (not (string-match "\\.dir-locals\\.el$"  buffer-file-name))
+               (string-match "/.*\\.el$"  buffer-file-name)
+               ;; (string-match init-file-user buffer-file-name)) ;; XXX: doesn't work
+               (file-newer-than-file-p buffer-file-name
+                                       (concat buffer-file-name "c")))
+      (when (file-exists-p (concat buffer-file-name ".elc"))
+        (delete-file (concat buffer-file-name ".elc")))
+      (byte-compile-file buffer-file-name))))
 
 (defun my-emacs-lisp-mode-hook ()
   (my-coding-keys emacs-lisp-mode-map)
