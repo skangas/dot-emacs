1e4ff884b1c07fd5afb9738c74ca1517c84c21f5
Stefan Kangas <stefankangas@gmail.com>
2023-07-29 13:55:56 +0200

    Add lisp/.dir-locals.el

---
+++ lisp/.dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((checkdoc-force-docstrings-flag . nil)
+         (checkdoc--argument-missing-flag . nil)
+         (checkdoc--disambiguate-symbol-flag . nil)
+         (checkdoc--interactive-docstring-flag . nil))))
